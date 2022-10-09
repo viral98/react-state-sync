355677d2eaa0a313af09b92b3fb3460dd3d2a35a
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-10-09 15:42:52 -0700

    Add cache service

--- react-state-sync/src/resources/BaseCacheResource.ts
+++ react-state-sync/src/resources/BaseCacheResource.ts
@@ -1,7 +1,5 @@
-export abstract class BaseCacheResource<T> {
+export  class BaseCacheResource<T> {
   protected cache: Map<string, T> | undefined;
-
-  protected abstract fetch(): Promise<T>;
 
   public async get(): Promise<T> {
    throw new Error("Not implemented");
