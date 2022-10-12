ec6a7f945e8ac0335dbbb148fa91acd7ed624738
Viral Tagdiwala <tagdiwalaviral@gmail.com>
2022-10-12 16:04:02 -0700

    Add connection to the database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,55 @@
+import { useEffect } from 'react'
 import store, { type ValuesStore } from '../src/store'
+import { Book } from '../src/types/books'
 
-export function getServerSideProps() {
-  return {
-    props: {
-      initialState: {
-        value1: 0,
-        value2: 0
-      }
-    }
-  }
+const DisplayValue = () => {
+  const val = store.useStore((state) => state)
+
+  return (
+    <div>
+      <h2>Books</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {val?.books.map((book: Book) => (
+            <tr key={book.id}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
-
-const DisplayValue = ({ item }: { item: keyof ValuesStore }) => (
-  <div>
-    {item}: {store.useStore((state) => state[item])}
-  </div>
-)
-
-const IncrementValue = ({ item }: { item: keyof ValuesStore }) => (
-  <button
-    onClick={() => {
-      const state = store.getState()
-
-      store.setState({
-        ...state,
-        [item]: state[item] + 1
-      })
-    }}>
-    Increment {item}
-  </button>
-)
 
 function App({ initialState }: { initialState: ValuesStore }) {
   store.serverInitialize(initialState)
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-extra-semi
+    ;(async () => {
+      console.log(process.env.NEXT_PUBLIC_API_URL)
+      const books = await fetch(process.env.NEXT_PUBLIC_API_URL + 'books')
+
+      store.setState({ books: (await books.json()) as unknown as Book[] })
+    })()
+  }, [])
+
   return (
     <div
       style={{
         display: 'grid',
         gridTemplateColumns: '1fr 1fr',
         maxWidth: 600,
-        gap: '1rem'
+        gap: '2rem'
       }}>
-      <IncrementValue item="value1" />
-      <DisplayValue item="value1" />
-      <IncrementValue item="value2" />
-      <DisplayValue item="value2" />
+      <DisplayValue />
     </div>
   )
 }
---
+++ src/types/books.ts
@@ -0,0 +1,10 @@
+export interface Book {
+  id: string
+  title: string
+  isbn: string
+  author: string
+  description?: string
+  published_date?: string
+  publisher?: string
+  updated_date?: string
+}
