01c148944fe4b5ad47d768356c31dc0d94033bc3
Parth Patel <parthjpatel9199@gmail.com>
2022-11-26 13:19:21 -0800

    Fixed context API tests

--- src/test-app/tests/BookComponentContext.test.tsx
+++ src/test-app/tests/BookComponentContext.test.tsx
@@ -8,39 +8,12 @@ describe('Context API calls', () => {
         json: () => Promise.resolve({ test: 100 })
       })
     ) as jest.Mock
+    const { rerender } = render(<BookComponentContainer />)
 
     for (let i = 0; i < 1000; i++) {
-      render(<BookComponentContainer />)
+      rerender(<BookComponentContainer />)
     }
 
     expect(fetch).toHaveBeenCalledTimes(1000)
-  })
-
-  it('Calls the API 100 times', async () => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve({ test: 100 })
-      })
-    ) as jest.Mock
-
-    for (let i = 0; i < 100; i++) {
-      render(<BookComponentContainer />)
-    }
-
-    expect(fetch).toHaveBeenCalledTimes(100)
-  })
-
-  it('Calls the API 10 times', async () => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve({ test: 100 })
-      })
-    ) as jest.Mock
-
-    for (let i = 0; i < 10; i++) {
-      render(<BookComponentContainer />)
-    }
-
-    expect(fetch).toHaveBeenCalledTimes(10)
   })
 })
